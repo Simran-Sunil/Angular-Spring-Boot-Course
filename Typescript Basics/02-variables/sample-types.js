3e3ac191db52acd76a1061dc0e06be2201ffa32c
Simran-Sunil <simrans.cs18@sahyadri.edu.in>
2023-01-18 15:43:09 +0530

    first commit

---
+++ Typescript Basics/02-variables/sample-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Simran";
+var lastName = "Sunil";
+console.log(found);
+console.log("The grade is: " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// use Template Strings
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ Typescript Basics/02-variables/sample-types.ts
@@ -0,0 +1,11 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: String = "Simran";
+let lastName: String = "Sunil";
+
+console.log(found);
+console.log("The grade is: " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+// use Template Strings
+console.log(`Hi ${firstName} ${lastName}`);
